7a11fed1204dd42bd25653963511c609a20a9490
杜晓浩 <1923911906@qq.com>
2022-08-14 16:52:51 +0800

    测试版00

--- read.py
+++ read.py
@@ -15,9 +15,10 @@ def read(num):
         content = [0] * len(first_page.extract_words())
         while i < len(first_page.extract_words()):
             content[i] = first_page.extract_words()[i]['text']
-            print(content[i])
+            # print(content[i])
             i = i + 1
         i = 1
+        # print(content)
         while i < len(content):
             if (content[i][len(content[i]) - 2] == ')') | (content[i][0] == ')') | (
                     (re.findall(pattern1, content[i]) != []) & (len(content[i]) <= 4)):
@@ -30,9 +31,10 @@ def read(num):
                     content[tag] = content[tag + 1]
             i = i + 1
         i = 0
+        # print(content)
         while i < len(content) - 1:
             if (content[i] == content[i + 1]) & (content[i] == content[len(content) - 2]):
-                for tag in range(i + 1, len(content)):
+                for tag in range(i, len(content)):
                     content[tag] = ''
             i = i + 1
         i = 0
@@ -44,6 +46,8 @@ def read(num):
                     text2 = text2 + content[i]
             i = i + 1
         text = text1 + text2
+        # print(text1)
+        # print(text2)
         return text
 
 
@@ -63,7 +67,7 @@ def split():
         i = 0
         all_list = []
         split_1 = re.compile(
-            r'(正确答案：[A-E]+(?!、)|正确答案：[√×]|A、[^0-9B-CE正]+|B、[^0-9ACDE正]+|C、[^0-9ABDE正]+|D、[^0-9A-CE正]+|[0-9]+[^A-E正√×]+)')
+            r'(正确答案：[A-E]+(?!、)|正确答案：[√×]|[A-E]、[^0-9A-E正]+|[0-9]+[^A-E正√×]+|[A-E]、[^0-9A-E正]+[0-9]{4}[^0-9A-E正]+)')
         res = re.split(split_1, texts)
 
         while None in res:  # 判断是否有空值在列表中
@@ -91,5 +95,5 @@ def split():
                 continue
             my_list.append(res[i])
             i = i + 1
-        # print(all_list)
-        print(read(16))
+        print(all_list)
+        # print(read(34))
